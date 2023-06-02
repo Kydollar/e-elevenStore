b3e7290acf0507a3ad845efc65ec2ff9ea750019
Kydollar <rizkywidodocuan@gmail.com>
2023-06-02 11:14:20 +0700

    update: recentSearch

--- elevenStore-main/src/components/Containers/Navigation/Topbar/search.js
+++ elevenStore-main/src/components/Containers/Navigation/Topbar/search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { InputBase, IconButton, Divider } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import ClearIcon from "@mui/icons-material/Clear";
@@ -8,7 +8,9 @@ import { Link } from "react-router-dom";
 const SearchPage = ({ onSearch, handleClose }) => {
 	const [searchText, setSearchText] = useState("");
 	const [product, setProduct] = useState([]);
-	const [msgError, setMsgError] = useState();
+	const [msgError, setMsgError] = useState("");
+	const [recentSearches, setRecentSearches] = useState([]);
+	const inputRef = useRef(null);
 
 	const handleSearch = () => {
 		onSearch(searchText);
@@ -22,41 +24,90 @@ const SearchPage = ({ onSearch, handleClose }) => {
 		setSearchText("");
 	};
 
-	const handleKeyDown = (event) => {
-		if (event.key === "Escape") {
-			handleClose();
-		}
-	};
+	const handleKeyDown = useCallback(
+		(event) => {
+			if (event.key === "Escape") {
+				handleClose();
+			}
+		},
+		[handleClose]
+	);
 
 	useEffect(() => {
 		document.addEventListener("keydown", handleKeyDown);
 		return () => {
 			document.removeEventListener("keydown", handleKeyDown);
 		};
-	}, []);
+	}, [handleKeyDown]);
 
 	useEffect(() => {
-		const timer = setTimeout(() => {
-			performSearch();
-		}); // Adjust the delay (in milliseconds) according to your needs
+		const storedSearches = localStorage.getItem("recentSearches");
+		if (storedSearches) {
+			setRecentSearches(JSON.parse(storedSearches));
+		}
+	}, []);
 
-		return () => {
-			clearTimeout(timer);
-		};
-	}, [searchText]);
+	const updateRecentSearches = useCallback((newSearch) => {
+		setRecentSearches((prevSearches) => {
+			const existingSearch = prevSearches.find(
+				(search) => search.nameProduct === newSearch.nameProduct
+			);
 
-	const performSearch = async () => {
+			if (existingSearch) {
+				// jika searchtext yang dicari ada masuk kefront localstorage
+				const updatedSearches = [
+					newSearch,
+					...prevSearches
+						.filter((search) => search.nameProduct !== newSearch.nameProduct)
+						.slice(0, 4),
+				];
+				localStorage.setItem("recentSearches", JSON.stringify(updatedSearches));
+				return updatedSearches;
+			} else {
+				// jika search yang dicari baru masuk kefront
+				const updatedSearches = [newSearch, ...prevSearches.slice(0, 4)];
+				localStorage.setItem("recentSearches", JSON.stringify(updatedSearches));
+				return updatedSearches;
+			}
+		});
+	}, []);
+
+	const performSearch = useCallback(async () => {
 		try {
 			const response = await axios.get(
 				`${process.env.REACT_APP_MY_API}/product/search?q=${searchText}`
 			);
-			// Handle the response and perform any necessary actions
-			setProduct(response.data);
-			setMsgError(response.data.msg);
+			const searchResult = response.data;
+
+			// Update terakhir yang dicari
+			if (searchResult.length > 0) {
+				const recentSearch = {
+					nameProduct: searchResult[0].nameProduct,
+					imageUrl: searchResult[0].imageUrl,
+				};
+				updateRecentSearches(recentSearch);
+			}
+
+			setProduct(searchResult);
+			setMsgError(searchResult.msg);
 		} catch (error) {
 			console.log(error);
 		}
-	};
+	}, [searchText, updateRecentSearches]);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			performSearch();
+		}, 100);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [searchText, performSearch]);
+
+	useEffect(() => {
+		inputRef.current.focus();
+	}, []);
 
 	return (
 		<div className="fixed bg-white rounded-md shadow-md flex flex-col md:w-[50vw] w-[96vw] -translate-x-1/2 p-2">
@@ -66,10 +117,11 @@ const SearchPage = ({ onSearch, handleClose }) => {
 				</IconButton>
 				<InputBase
 					className="flex-1"
-					placeholder="Search..."
+					placeholder="Search product..."
 					value={searchText}
 					onChange={handleChange}
 					sx={{ backgroundColor: "white", borderRadius: "4px", padding: "4px" }}
+					inputRef={inputRef}
 				/>
 				{searchText && (
 					<IconButton onClick={handleClear}>
@@ -84,23 +136,54 @@ const SearchPage = ({ onSearch, handleClose }) => {
 				</span>
 			</div>
 			<Divider />
-			{msgError && (
-				<div className="bg-white p-2 py-6 flex gap-4 justify-center items-center">
-					<p className="text-gray-400 text-lg">{msgError}</p>
+			{searchText ? (
+				<>
+					{msgError && (
+						<div className="bg-white p-2 py-6 flex gap-4 justify-center items-center">
+							<p className="text-gray-400 text-lg">{msgError}</p>
+						</div>
+					)}
+					{Array.isArray(product) &&
+						product.map((product) => (
+							<Link
+								to={`/products/${product?.product_category.productCategoryName}/${product?.uuid}`}
+								key={product?.uuid}
+								onClick={handleClose}
+								className="bg-white p-2 flex gap-4 cursor-pointer justify-start items-center hover:bg-gray-100"
+							>
+								<img
+									src={product?.imageUrl}
+									className="w-16 h-16 object-cover bg-white"
+									alt={product?.nameProduct}
+								/>
+								<h1 className="cursor-pointer">{product?.nameProduct}</h1>
+							</Link>
+						))}
+				</>
+			) : (
+				<div className="bg-white p-2">
+					<h2 className="text-gray-600 mb-2">Terakhir yang anda cari:</h2>
+					{recentSearches.map((search, index) => (
+						<button
+							key={index}
+							className="bg-gray-100 rounded-md px-2 py-1 mr-2 mb-2 text-sm hover:bg-gray-200"
+							onClick={() => {
+								setSearchText(search.nameProduct);
+								performSearch();
+							}}
+						>
+							<div className="flex items-center">
+								<img
+									src={search.imageUrl}
+									alt={search.nameProduct}
+									className="w-8 h-8 object-cover rounded-full mr-2"
+								/>
+								<p>{search.nameProduct}</p>
+							</div>
+						</button>
+					))}
 				</div>
 			)}
-			{Array.isArray(product) &&
-				product.map((product) => (
-					<Link
-						to={`/products/${product?.product_category.productCategoryName}/${product?.uuid}`}
-						key={product?.uuid}
-						onClick={handleClose}
-						className="bg-white p-2 flex gap-4 cursor-pointer justify-start items-center hover:bg-gray-100"
-					>
-						<img src={product?.imageUrl} className="w-16 h-16 object-cover bg-white" />
-						<h1 className="cursor-pointer">{product?.nameProduct}</h1>
-					</Link>
-				))}
 		</div>
 	);
 };
