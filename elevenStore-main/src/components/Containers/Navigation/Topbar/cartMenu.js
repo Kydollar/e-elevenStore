3e1de8cc0446d2dd5820d1713e689d91456dc60b
David <dasantosa29@gmail.com>
2023-06-14 00:04:28 +0700

    edit

--- elevenStore-main/src/components/Containers/Navigation/Topbar/cartMenu.js
+++ elevenStore-main/src/components/Containers/Navigation/Topbar/cartMenu.js
@@ -7,87 +7,79 @@ import Button from "components/Button";
 import { formatter } from "utils/useFormatter";
 
 function CartMenu(props) {
-	const { anchorElCart, isCartMenuOpen, handleCartMenuClose, setCartLength } = props;
-	const { user } = useSelector((state) => state.auth);
-	const navigate = useNavigate();
-	const userUuid = user && user.uuid;
-	const [cart, setCart] = useState([]);
+  const { anchorElCart, isCartMenuOpen, handleCartMenuClose, setCartLength } = props;
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const userUuid = user && user.uuid;
+  const [cart, setCart] = useState([]);
 
-	useEffect(() => {
-		if (!userUuid) return;
+  useEffect(() => {
+    if (!userUuid) return;
 
-		const getCart = async () => {
-			try {
-				const response = await axios.get(
-					`${process.env.REACT_APP_MY_API}/cart/${userUuid}?statusActive=true`
-				);
-				setCart(response.data);
-			} catch (error) {
-				console.error("Error fetching cart:", error);
-			}
-		};
+    const getCart = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_MY_API}/cart/${userUuid}?statusActive=true`);
+        setCart(response.data);
+      } catch (error) {
+        console.error("Error fetching cart:", error);
+      }
+    };
 
-		getCart();
+    getCart();
 
-		if (isCartMenuOpen) {
-			getCart();
-		}
-	}, [userUuid, isCartMenuOpen]);
+    if (isCartMenuOpen) {
+      getCart();
+    }
+  }, [userUuid, isCartMenuOpen]);
 
-	useEffect(() => {
-		// Update the cart length whenever the cart changes
-		setCartLength(cart.length);
-	}, [cart, setCartLength]);
+  useEffect(() => {
+    // Update the cart length whenever the cart changes
+    setCartLength(cart.length);
+  }, [cart, setCartLength]);
 
-	return (
-		<Menu
-			anchorEl={anchorElCart}
-			sx={{ top: "3rem" }}
-			anchorOrigin={{
-				vertical: "top",
-				horizontal: "right",
-			}}
-			id="cart-menu"
-			keepMounted
-			transformOrigin={{
-				vertical: "top",
-				horizontal: "right",
-			}}
-			open={isCartMenuOpen}
-			onClose={handleCartMenuClose}
-		>
-			<div className="py-1 px-4">
-				{cart.length <= 0 ? (
-					<h1>Keranjang anda masih kosong, yuk belanja</h1>
-				) : (
-					<h1>Anda Memiliki {cart.length} items dikeranjang</h1>
-				)}
-			</div>
-			{cart.map((item) => (
-				<MenuItem key={item.uuid} onClick={handleCartMenuClose}>
-					<div className="flex items-center gap-2 text-sm">
-						<img className="w-10 h-10" src={item.product.imageUrl} alt={item.product.nameProduct} />
-						<h1>{item.product.nameProduct}</h1>
-						<h1 className="text-blue-500">{formatter.format(item.product.price)}</h1>
-					</div>
-				</MenuItem>
-			))}
-			<div className="py-2 px-4">
-				{cart.length <= 0 ? null : (
-					<Button
-						onClick={() => {
-							navigate("/cart");
-							handleCartMenuClose();
-						}}
-						inputClassName="w-full justify-center"
-						secondary
-					>
-						Tampilkan Keranjang Belanja
-					</Button>
-				)}
-			</div>
-		</Menu>
-	);
+  return (
+    <Menu
+      anchorEl={anchorElCart}
+      sx={{ top: "3rem" }}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id="cart-menu"
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isCartMenuOpen}
+      onClose={handleCartMenuClose}
+    >
+      <div className="py-1 px-4">{cart.length <= 0 ? <h1>Keranjang anda masih kosong, yuk belanja</h1> : <h1>Anda Memiliki {cart.length} items dikeranjang</h1>}</div>
+      {cart.map((item) => (
+        <MenuItem key={item.uuid} onClick={handleCartMenuClose}>
+          <div className="flex items-center gap-2 text-sm">
+            <img className="w-10 h-10" src={item.product.imageUrl} alt={item.product.nameProduct} />
+            <h1>{item.product.nameProduct}</h1>
+            <h1 className="text-blue-500">{formatter.format(item.product.price)}</h1>
+          </div>
+        </MenuItem>
+      ))}
+      <div className="py-2 px-4">
+        {cart.length <= 0 ? null : (
+          <Button
+            onClick={() => {
+              navigate("/cart");
+              handleCartMenuClose();
+            }}
+            inputClassName="w-full justify-center"
+            secondary
+          >
+            Tampilkan Keranjang Belanja
+          </Button>
+        )}
+      </div>
+    </Menu>
+  );
 }
 
 export default CartMenu;
--- elevenStore-main/src/components/Containers/Navigation/Topbar/index.jsx
+++ elevenStore-main/src/components/Containers/Navigation/Topbar/index.jsx
@@ -22,228 +22,189 @@ import SearchPage from "./search";
 const drawerWidth = 280;
 
 const AppBar = styled(MuiAppBar, {
-	shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-	zIndex: theme.zIndex.drawer + 1,
-	transition: theme.transitions.create(["width", "margin"], {
-		easing: theme.transitions.easing.sharp,
-		duration: theme.transitions.duration.leavingScreen,
-	}),
-	...(open && {
-		marginLeft: drawerWidth,
-		width: `calc(100% - ${drawerWidth}px)`,
-		transition: theme.transitions.create(["width", "margin"], {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.enteringScreen,
-		}),
-	}),
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
 }));
 
 const Search = styled("div")(({ theme }) => ({
-	position: "relative",
-	borderRadius: theme.shape.borderRadius,
-	backgroundColor: alpha(theme.palette.common.white, 0.15),
-	"&:hover": {
-		backgroundColor: alpha(theme.palette.common.white, 0.25),
-	},
-	marginLeft: 0,
-	width: "100%",
-	[theme.breakpoints.up("sm")]: {
-		marginLeft: theme.spacing(1),
-		width: "auto",
-	},
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
 }));
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
-	padding: theme.spacing(0, 2),
-	height: "100%",
-	position: "absolute",
-	pointerEvents: "none",
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-	color: "inherit",
-	"& .MuiInputBase-input": {
-		padding: theme.spacing(1, 1, 1, 0),
-		// vertical padding + font size from searchIcon
-		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-		transition: theme.transitions.create("width"),
-		width: "100%",
-		[theme.breakpoints.up("sm")]: {
-			width: "16ch",
-		},
-	},
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "16ch",
+    },
+  },
 }));
 
 function Topbar(props) {
-	const { open, handleDrawerOpen, scrollNav } = props;
-	const [anchorElProfile, setAnchorElProfile] = React.useState(null);
-	const [anchorElCart, setAnchorElCart] = React.useState(null);
-	const [cartLength, setCartLength] = React.useState(0);
-	const [openModalLogin, setOpenModalLogin] = React.useState(false);
-	const [openSearchModal, setOpenSearchModal] = React.useState(false);
+  const { open, handleDrawerOpen, scrollNav } = props;
+  const [anchorElProfile, setAnchorElProfile] = React.useState(null);
+  const [anchorElCart, setAnchorElCart] = React.useState(null);
+  const [cartLength, setCartLength] = React.useState(0);
+  const [openModalLogin, setOpenModalLogin] = React.useState(false);
+  const [openSearchModal, setOpenSearchModal] = React.useState(false);
 
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
 
-	React.useEffect(() => {
-		dispatch(reset());
-	}, [dispatch]);
+  React.useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
 
-	const isProfileMenuOpen = Boolean(anchorElProfile);
-	const isCartMenuOpen = Boolean(anchorElCart);
+  const isProfileMenuOpen = Boolean(anchorElProfile);
+  const isCartMenuOpen = Boolean(anchorElCart);
 
-	const handleProfileMenuOpen = (event) => {
-		setAnchorElProfile(event.currentTarget);
-	};
+  const handleProfileMenuOpen = (event) => {
+    setAnchorElProfile(event.currentTarget);
+  };
 
-	const handleProfileMenuClose = () => {
-		setAnchorElProfile(null);
-	};
+  const handleProfileMenuClose = () => {
+    setAnchorElProfile(null);
+  };
 
-	const handleCartMenuOpen = (event) => {
-		setAnchorElCart(event.currentTarget);
-	};
+  const handleCartMenuOpen = (event) => {
+    setAnchorElCart(event.currentTarget);
+  };
 
-	const handleCartMenuClose = () => {
-		setAnchorElCart(null);
-	};
+  const handleCartMenuClose = () => {
+    setAnchorElCart(null);
+  };
 
-	const handleOpenLogin = () => {
-		setOpenModalLogin(true);
-	};
+  const handleOpenLogin = () => {
+    setOpenModalLogin(true);
+  };
 
-	const handleCloseLogin = () => {
-		setOpenModalLogin(false);
-	};
+  const handleCloseLogin = () => {
+    setOpenModalLogin(false);
+  };
 
-	const handleSearchClick = () => {
-		setOpenSearchModal(true);
-	};
+  const handleSearchClick = () => {
+    setOpenSearchModal(true);
+  };
 
-	const handleSearchClose = () => {
-		setOpenSearchModal(false);
-	};
+  const handleSearchClose = () => {
+    setOpenSearchModal(false);
+  };
 
-	const logout = () => {
-		dispatch(LogOut());
-		dispatch(reset());
-		navigate("/");
-	};
+  const logout = () => {
+    dispatch(LogOut());
+    dispatch(reset());
+    navigate("/");
+  };
 
-	const profileMenuId = "profile-menu";
-	const cartMenuId = "cart-menu";
+  const profileMenuId = "profile-menu";
+  const cartMenuId = "cart-menu";
 
-	return (
-		<>
-			<AppBar
-				position="fixed"
-				open={open}
-				// className={`!bg-transparent !shadow-none backdrop-blur-xl border border-gray-200 ${open && "rounded-l-xl"}`}
-				className={`!bg-transparent bg-gradient-to-tr from-gray-800/30 to-blue-800/50 !shadow-md backdrop-blur-xl ${
-					open && "rounded-l-xl"
-				}`}
-			>
-				<Toolbar>
-					<IconButton
-						color="inherit"
-						aria-label="open drawer"
-						onClick={handleDrawerOpen}
-						edge="start"
-						sx={{
-							marginRight: 5,
-							...(open && { display: "none" }),
-						}}
-					>
-						<MenuIcon />
-					</IconButton>
-					<div className="text-white">Breadcrumbs / Notyet</div>
-					<Box sx={{ flexGrow: 1 }} />
-					<Search
-						onClick={handleSearchClick}
-						className="mr-4 text-gray-500 border-gray-200/50 border !bg-white/70 !rounded-lg"
-						style={{ boxShadow: "0px 0px 5px 2px rgba(0,0,0,0.03)" }}
-					>
-						<SearchIconWrapper>
-							<SearchIcon />
-						</SearchIconWrapper>
-						<StyledInputBase
-							placeholder="Search products…"
-							inputProps={{ "aria-label": "search" }}
-							readOnly
-						/>
-					</Search>
-					{user ? (
-						<>
-							<Box sx={{ display: { md: "flex" } }}>
-								{user?.role_category?.roleName !== "admin" && (
-									<IconButton
-										size="large"
-										aria-label="show shopping cart"
-										onClick={handleCartMenuOpen}
-										color="inherit"
-									>
-										<Badge badgeContent={cartLength} color="error">
-											<ShoppingCartIcon />
-										</Badge>
-									</IconButton>
-								)}
-								<IconButton
-									size="large"
-									edge="end"
-									aria-label="account of current user"
-									aria-controls={profileMenuId}
-									aria-haspopup="true"
-									onClick={handleProfileMenuOpen}
-									color="inherit"
-								>
-									<Avatar
-										src={user?.avatarUrl}
-										alt={`${user?.name} Avatar`}
-										sx={{ width: 26, height: 26 }}
-									/>
-								</IconButton>
-							</Box>
-						</>
-					) : (
-						<div className="inline-flex gap-2">
-							<Button primary onClick={handleOpenLogin}>
-								Masuk
-							</Button>
-							<Link to={"/register"} className="inline-flex">
-								<Button secondary>Daftar</Button>
-							</Link>
-						</div>
-					)}
-				</Toolbar>
-			</AppBar>
-			<ProfileMenu
-				anchorEl={anchorElProfile}
-				isProfileMenuOpen={isProfileMenuOpen}
-				handleProfileMenuClose={handleProfileMenuClose}
-				logout={logout}
-				profileMenuId={profileMenuId}
-			/>
-			<CartMenu
-				anchorEl={anchorElCart}
-				isCartMenuOpen={isCartMenuOpen}
-				handleCartMenuClose={handleCartMenuClose}
-				cartMenuId={cartMenuId}
-				setCartLength={setCartLength}
-			/>
-			<CustomModal open={openModalLogin} handleClose={handleCloseLogin}>
-				{/* Modal Content */}
-				<Login handleCloseLogin={handleCloseLogin} />
-			</CustomModal>
-			<CustomModal open={openSearchModal} handleClose={handleSearchClose} onTop>
-				<SearchPage handleClose={handleSearchClose} />
-			</CustomModal>
-		</>
-	);
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        open={open}
+        // className={`!bg-transparent !shadow-none backdrop-blur-xl border border-gray-200 ${open && "rounded-l-xl"}`}
+        className={`!bg-transparent bg-gradient-to-tr from-gray-800/30 to-blue-800/50 !shadow-md backdrop-blur-xl ${open && "rounded-l-xl"}`}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          {/* <div className="text-white">Breadcrumbs / Notyet</div> */}
+          <Box sx={{ flexGrow: 1 }} />
+          <Search onClick={handleSearchClick} className="mr-4 text-gray-500 border-gray-200/50 border !bg-white/70 !rounded-lg" style={{ boxShadow: "0px 0px 5px 2px rgba(0,0,0,0.03)" }}>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search products…" inputProps={{ "aria-label": "search" }} readOnly />
+          </Search>
+          {user ? (
+            <>
+              <Box sx={{ display: { md: "flex" } }}>
+                {user?.role_category?.roleName !== "admin" && (
+                  <IconButton size="large" aria-label="show shopping cart" onClick={handleCartMenuOpen} color="inherit">
+                    <Badge badgeContent={cartLength} color="error">
+                      <ShoppingCartIcon />
+                    </Badge>
+                  </IconButton>
+                )}
+                <IconButton size="large" edge="end" aria-label="account of current user" aria-controls={profileMenuId} aria-haspopup="true" onClick={handleProfileMenuOpen} color="inherit">
+                  <Avatar src={user?.avatarUrl} alt={`${user?.name} Avatar`} sx={{ width: 26, height: 26 }} />
+                </IconButton>
+              </Box>
+            </>
+          ) : (
+            <div className="inline-flex gap-2">
+              <Button primary onClick={handleOpenLogin}>
+                Masuk
+              </Button>
+              <Link to={"/register"} className="inline-flex">
+                <Button secondary>Daftar</Button>
+              </Link>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+      <ProfileMenu anchorEl={anchorElProfile} isProfileMenuOpen={isProfileMenuOpen} handleProfileMenuClose={handleProfileMenuClose} logout={logout} profileMenuId={profileMenuId} />
+      <CartMenu anchorEl={anchorElCart} isCartMenuOpen={isCartMenuOpen} handleCartMenuClose={handleCartMenuClose} cartMenuId={cartMenuId} setCartLength={setCartLength} />
+      <CustomModal open={openModalLogin} handleClose={handleCloseLogin}>
+        {/* Modal Content */}
+        <Login handleCloseLogin={handleCloseLogin} />
+      </CustomModal>
+      <CustomModal open={openSearchModal} handleClose={handleSearchClose} onTop>
+        <SearchPage handleClose={handleSearchClose} />
+      </CustomModal>
+    </>
+  );
 }
 
 export default Topbar;
